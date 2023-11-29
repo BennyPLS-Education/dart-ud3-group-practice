acdfe14ef5dcf752a49ee363c53c67167d33af0f
Benny <peptoni0345@gmail.com>
2023-11-29 21:52:58 +0100

    Add routing configuration and theme updates
    A new routing configuration has been added in routes.dart for ease of navigation and maintainability.
    
    The configuration uses an enumeration to define routes, which significantly reduces chances of runtime errors caused by mistyping paths.
    
    This commit also includes some theme updates in main.dart, where a theme function is created to allow color scheme changes throughout the app. It implements the Material3 design and sets a dark color scheme with a red accent for the primary color.

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:group_practice/bloc/shop_bloc.dart';
+import 'package:group_practice/routes/routes.dart';
 
 import 'bloc/login_bloc.dart';
 import 'screens/screens.dart';
 
-void main() {
-  runApp(const AppState());
-}
+void main() => runApp(const AppState());
 
 class AppState extends StatelessWidget {
-  const AppState({super.key});
+  const AppState() : super(key: null);
 
   @override
   Widget build(BuildContext context) {
-    return MultiBlocProvider(providers: [
-      BlocProvider<LoginBloc>(
-        create: (context) => LoginBloc(),
-      ),
-      BlocProvider<ShopBloc>(
-        create: (context) => ShopBloc(),
-      ),
-    ], child: const App());
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<LoginBloc>(
+          create: (context) => LoginBloc(),
+        ),
+        BlocProvider<ShopBloc>(
+          create: (context) => ShopBloc(),
+        ),
+      ],
+      child: const App(),
+    );
   }
 }
 
@@ -33,14 +35,21 @@ class App extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Shopping App (Bloc Demo)',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      routes: {
-        '/': (context) => const HomeScreen(),
-        'logOrReg': (context) => LoginOrRegisterScreen(),
-      },
+      theme: getTheme(),
+      routes: getRoutes(),
       initialRoute: 'logOrReg',
+    );
+  }
+
+  getTheme() {
+    return ThemeData(
+        useMaterial3: true,
+        colorScheme: const ColorScheme.dark(
+          primary: Colors.redAccent,
+        )).copyWith(
+      appBarTheme: const AppBarTheme(
+        color: Colors.redAccent,
+      ),
     );
   }
 }
---
+++ lib/routes/routes.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+import '../screens/screens.dart';
+
+extension RoutesExtension on Location {
+  static const String fallback = '/';
+
+  /// Returns a `String` representation of the route.
+  String get path {
+    var route = _routeMap[this];
+    if (route == null) return fallback;
+    return route;
+  }
+}
+
+enum Location {
+  home,
+  logOrReg,
+}
+
+final _routeMap = {
+  Location.home: '/',
+  Location.logOrReg: 'logOrReg',
+};
+
+final _routes = <String, WidgetBuilder>{
+  '/': (context) => const HomeScreen(),
+  'logOrReg': (context) => LoginOrRegisterScreen(),
+};
+
+Map<String, WidgetBuilder> getRoutes() => _routes;
