83e726b5a604028b1a447c202a66c53686a908bc
Benny <peptoni0345@gmail.com>
2023-11-29 22:30:41 +0100

    Refactor login/register screen for code readability and UX
    Decomposed the login/register screen structure into single widget methods, improving code readability and maintainability. This also included creating separate form validation methods to decrease code repetition. Unused variables and the AnimationController and its dependencies have also been removed for cleaner, simpler code. Furthermore, a form status widget was added for user feedback on incorrect login. This change not only improves the code quality but also enhances the user experience.

--- lib/screens/login_or_register.dart
+++ lib/screens/login_or_register.dart
@@ -7,206 +7,195 @@ import 'package:group_practice/events/events.dart';
 import 'package:group_practice/states/states.dart';
 
 class LoginOrRegisterScreen extends StatefulWidget {
+  const LoginOrRegisterScreen() : super(key: null);
+
   @override
   _LoginOrRegisterScreenState createState() => _LoginOrRegisterScreenState();
 }
 
-class _LoginOrRegisterScreenState extends State<LoginOrRegisterScreen>
-    with SingleTickerProviderStateMixin {
-  late AnimationController controller;
-  late Animation<double> animation;
+class _LoginOrRegisterScreenState extends State<LoginOrRegisterScreen> {
   final GlobalKey<FormState> _key = GlobalKey();
 
-  String user = 't@t.com';
-  String password = 'CalaClara';
+  bool _isLoading = false;
+  bool? _isValid;
 
-  bool isLogin = false;
-  bool isRegister = false;
-  late final List<bool> _selectedEvent = [isLogin, isRegister];
-
-  RegExp emailRegExp = RegExp(r'^\w+[\w-\.]*\@\w+((-\w+)|(\w*))\.[a-z]{2,3}$');
+  RegExp emailRegExp = RegExp(r'^\w+[\w-.]*@\w+((-\w+)|(\w*))\.[a-z]{2,3}$');
   RegExp contRegExp = RegExp(r'^([1-zA-Z0-1@.\s]{1,255})$');
   String? _correu;
   String? _passwd;
-  String missatge = '';
-  bool _isChecked = false;
-
-  bool _isLoading = false;
+  bool _rememberUserCheck = false;
 
   StreamSubscription<LoginState>? loginSubscription;
 
   @override
-  initState() {
-    super.initState();
-    controller = AnimationController(
-      duration: const Duration(milliseconds: 1000),
-      vsync: this,
-    );
-    animation = CurvedAnimation(parent: controller, curve: Curves.easeIn);
-
-    //Descomentar las siguientes lineas para generar un efecto de "respiracion"
-    /*animation.addStatusListener((status) {
-      if (status == AnimationStatus.completed) {
-        controller.reverse();
-      } else if (status == AnimationStatus.dismissed) {
-        controller.forward();
-      }
-    });*/
-    controller.forward();
-  }
-
-  @override
   dispose() {
-    // Es important SEMPRE realitzar el dispose del controller.
-    controller.dispose();
+    loginSubscription?.cancel();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    final bloc = BlocProvider.of<LoginBloc>(context);
-    loginSubscription = bloc.stream.listen((LoginState state) {
-      if (state.isValid ?? false) {
-        Navigator.of(context).pushReplacementNamed('/');
-        loginSubscription!.cancel();
-      } else {
-        print('No es valid');
-      }
-    });
+    final provider = BlocProvider.of<LoginBloc>(context);
+    loginSubscription = provider.stream.listen(_streamListener);
 
     return Scaffold(
       resizeToAvoidBottomInset: false,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            SizedBox(
-              height: 200,
-              child: AnimatedLogo(animation: animation),
-            ),
-            if (isLogin || isRegister) loginOrRegisterForm(),
-            const SizedBox(height: 100),
-            loginOrRegister()
-          ],
-        ),
-      ),
+      body: _body(),
     );
   }
 
-  Widget loginOrRegister() {
-    return ToggleButtons(
-      direction: Axis.horizontal,
-      onPressed: (int index) {
-        setState(() {
-          // The botó que està seleccionat esta a true, tots els altres a false
-          for (int i = 0; i < _selectedEvent.length; i++) {
-            _selectedEvent[i] = i == index;
-          }
-          if (index == 0) {
-            isLogin = true;
-            isRegister = false;
-          } else {
-            isLogin = false;
-            isRegister = true;
-          }
-        });
-      },
-      borderRadius: const BorderRadius.all(Radius.circular(8)),
-      selectedBorderColor: Colors.blue[800],
-      selectedColor: Colors.white,
-      fillColor: Colors.blue[200],
-      color: Colors.blue[400],
-      constraints: const BoxConstraints(
-        minHeight: 40.0,
-        minWidth: 120.0,
+  void _streamListener(LoginState state) {
+    if (state.isValid ?? false) {
+      Navigator.of(context).pushReplacementNamed('/');
+    } else {
+      _isValid = false;
+    }
+
+    setState(() {
+      _isLoading = false;
+    });
+  }
+
+  Center _body() {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const SizedBox(
+            height: 200,
+            child: FlutterLogo(
+              size: 200,
+              textColor: Colors.redAccent,
+            ),
+          ),
+          loginOrRegisterForm(),
+          const SizedBox(height: 100),
+        ],
       ),
-      isSelected: _selectedEvent,
-      children: events,
     );
   }
 
   Widget loginOrRegisterForm() {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        Text(isLogin ? 'Inicia sessió' : 'Registra\'t'),
+      children: [
+        const Text('Inicia sessió', style: TextStyle(fontSize: 30)),
         SizedBox(
           width: 300.0,
-          child: Form(
-            key: _key,
-            child: Column(
-              children: <Widget>[
-                TextFormField(
-                  initialValue: '',
-                  validator: (text) {
-                    if (text!.length == 0) {
-                      return "Correu es obligatori";
-                    } else if (!emailRegExp.hasMatch(text)) {
-                      return "Format correu incorrecte";
-                    }
-                    return null;
-                  },
-                  keyboardType: TextInputType.emailAddress,
-                  maxLength: 50,
-                  textAlign: TextAlign.left,
-                  decoration: InputDecoration(
-                    hintText: 'Escrigui el seu correu',
-                    labelText: 'Correu',
-                    counterText: '',
-                    icon:
-                        Icon(Icons.email, size: 32.0, color: Colors.blue[800]),
-                  ),
-                  onSaved: (text) => _correu = text,
-                ),
-                TextFormField(
-                  initialValue: '',
-                  validator: (text) {
-                    if (text!.isEmpty) {
-                      return "Contrasenya és obligatori";
-                    } else if (text.length <= 5) {
-                      return "Contrasenya mínim de 5 caràcters";
-                    } else if (!contRegExp.hasMatch(text)) {
-                      return "Contrasenya incorrecte";
-                    }
-                    return null;
-                  },
-                  keyboardType: TextInputType.text,
-                  maxLength: 20,
-                  textAlign: TextAlign.left,
-                  decoration: InputDecoration(
-                    hintText: 'Escrigui la contrasenya',
-                    labelText: 'Contrasenya',
-                    counterText: '',
-                    icon: Icon(Icons.lock, size: 32.0, color: Colors.blue[800]),
-                  ),
-                  onSaved: (text) => _passwd = text,
-                ),
-                isLogin
-                    ? CheckboxListTile(
-                        value: _isChecked,
-                        onChanged: (value) {
-                          _isChecked = value!;
-                          setState(() {});
-                        },
-                        title: const Text('Recorda\'m'),
-                        controlAffinity: ListTileControlAffinity.leading,
-                      )
-                    : const SizedBox(height: 56),
-                IconButton(
-                  onPressed: () => _loginRegisterRequest(),
-                  icon: Icon(
-                    Icons.arrow_forward,
-                    size: 42.0,
-                    color: Colors.blue[800],
-                  ),
-                ),
-                _isLoading ? const CircularProgressIndicator() : Container(),
-              ],
-            ),
-          ),
+          child: _form(),
         ),
       ],
     );
+  }
+
+  Form _form() {
+    return Form(
+      key: _key,
+      child: Column(
+        children: [
+          _emailField(),
+          _passwdField(),
+          _rememberUserCheckbox(),
+          _submitButton(),
+          _formStatus(),
+        ],
+      ),
+    );
+  }
+
+  IconButton _submitButton() {
+    return IconButton(
+      onPressed: () => _loginRegisterRequest(),
+      icon: const Icon(Icons.arrow_forward, size: 42.0),
+    );
+  }
+
+  TextFormField _emailField() {
+    return TextFormField(
+      initialValue: '',
+      validator: _emailValidator,
+      keyboardType: TextInputType.emailAddress,
+      maxLength: 50,
+      textAlign: TextAlign.left,
+      decoration: const InputDecoration(
+        hintText: 'Escrigui el seu correu',
+        labelText: 'Correu',
+        counterText: '',
+        icon: Icon(Icons.email, size: 32.0),
+      ),
+      onSaved: (text) => _correu = text,
+    );
+  }
+
+  String? _emailValidator(String? text) {
+    if (text!.isEmpty) {
+      return "Correu es obligatori";
+    } else if (!emailRegExp.hasMatch(text)) {
+      return "Format correu incorrecte";
+    }
+
+    return null;
+  }
+
+  TextFormField _passwdField() {
+    return TextFormField(
+      initialValue: '',
+      validator: _passwdValidator,
+      keyboardType: TextInputType.text,
+      maxLength: 20,
+      textAlign: TextAlign.left,
+      decoration: const InputDecoration(
+        hintText: 'Escrigui la contrasenya',
+        labelText: 'Contrasenya',
+        counterText: '',
+        icon: Icon(Icons.lock, size: 32.0),
+      ),
+      onSaved: (text) => _passwd = text,
+    );
+  }
+
+  String? _passwdValidator(String? text) {
+    if (text!.isEmpty) {
+      return "Contrasenya és obligatori";
+    } else if (text.length <= 5) {
+      return "Contrasenya mínim de 5 caràcters";
+    } else if (!contRegExp.hasMatch(text)) {
+      return "Contrasenya incorrecte";
+    }
+
+    return null;
+  }
+
+  Widget _rememberUserCheckbox() {
+    return CheckboxListTile(
+      value: _rememberUserCheck,
+      onChanged: (value) {
+        setState(() {
+          _rememberUserCheck = value!;
+        });
+      },
+      title: const Text('Recorda\'m'),
+      controlAffinity: ListTileControlAffinity.leading,
+    );
+  }
+
+  Widget _formStatus() {
+    if (_isLoading) {
+      return const CircularProgressIndicator();
+    }
+
+    if (_isValid != null && !_isValid!) {
+      return const Text(
+        'Usuari o contrasenya incorrectes',
+        textAlign: TextAlign.center,
+        style: TextStyle(
+          color: Colors.redAccent,
+          fontSize: 16,
+        ),
+      );
+    }
+
+    return Container();
   }
 
   _loginRegisterRequest() async {
@@ -216,42 +205,8 @@ class _LoginOrRegisterScreenState extends State<LoginOrRegisterScreen>
         _isLoading = true;
       });
 
-      // Aquí es realitzaria la petició de login a l'API o similar
-      missatge = 'Gràcies \n $_correu \n $_passwd';
-      setState(() {
-        _isLoading = false;
-      });
-
       final provider = BlocProvider.of<LoginBloc>(context);
       provider.add(LoginEventRequest(_correu!, _passwd!));
     }
   }
 }
-
-class AnimatedLogo extends AnimatedWidget {
-  // Maneja los Tween estáticos debido a que estos no cambian.
-  static final _opacityTween = Tween<double>(begin: 0.1, end: 1.0);
-  static final _sizeTween = Tween<double>(begin: 0.0, end: 100.0);
-
-  AnimatedLogo({Key? key, required Animation<double> animation})
-      : super(key: key, listenable: animation);
-
-  @override
-  Widget build(BuildContext context) {
-    final Animation<double> animation = listenable as Animation<double>;
-    return Opacity(
-      opacity: _opacityTween.evaluate(animation),
-      child: Container(
-        margin: const EdgeInsets.symmetric(vertical: 10.0),
-        height: _sizeTween.evaluate(animation), // Aumenta la altura
-        width: _sizeTween.evaluate(animation), // Aumenta el ancho
-        child: const FlutterLogo(),
-      ),
-    );
-  }
-}
-
-const List<Widget> events = <Widget>[
-  Text('Inicia sessió'),
-  Text('Registra\'t'),
-];
