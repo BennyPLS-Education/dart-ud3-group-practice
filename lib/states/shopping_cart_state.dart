f1de736baddeb4bc89d26587e6ab1a52e16f7868
Benny <peptoni0345@gmail.com>
2023-11-29 22:46:56 +0100

    Remove unnecessary print statements in shopping cart state
    Removed two print statements in the shopping cart state that were used for debugging purposes, and added some whitespace for better readability of the code.
    
    These changes improve the cleanliness of the code and reduce unnecessary console output during runtime.

--- lib/states/shopping_cart_state.dart
+++ lib/states/shopping_cart_state.dart
@@ -13,18 +13,19 @@ class ShoppingCartState {
 
   ShoppingCartState copyAdd(Product product) {
     Map<Product, int> newProducts = Map.from(products);
-    print(newProducts.containsKey(product));
-    print(newProducts[product]);
+
     if (newProducts.containsKey(product)) {
       newProducts[product] = newProducts[product]! + 1;
     } else {
       newProducts[product] = 1;
     }
+
     return ShoppingCartState(newProducts);
   }
 
   ShoppingCartState copyRemove(Product product) {
     Map<Product, int> newProducts = Map.from(products);
+
     if (newProducts.containsKey(product)) {
       if (newProducts[product] == 1) {
         newProducts.remove(product);
@@ -32,6 +33,7 @@ class ShoppingCartState {
         newProducts[product] = newProducts[product]! - 1;
       }
     }
+
     return ShoppingCartState(newProducts);
   }
 }
