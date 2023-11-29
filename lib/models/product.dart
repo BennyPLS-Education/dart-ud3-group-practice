3e60bebbb68111a9a062c32b1ba88e325369ff66
Andreu Garcia <andreustreaming@gmail.com>
2023-11-29 21:07:57 +0100

    merge resolved

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -13,4 +13,18 @@ class Product {
 
   @override
   int get hashCode => price.hashCode ^ image.hashCode;
+
+  factory Product.fromRawJson(String str) => Product.fromJson(json.decode(str));
+
+  String toRawJson() => json.encode(toJson());
+
+  factory Product.fromJson(Map<String, dynamic> json) => Product(
+        image: json["image"],
+        price: json["price"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "image": image,
+        "price": price,
+      };
 }
